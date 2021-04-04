082238fce155f40cb107b77227e35a431637f97e
MakotoUwaya <makoto.uwaya@gmail.com>
2021-04-04 13:02:11 +0900

    install puppeteer

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log('Hello TypeScript world.');
+import puppeteer from 'puppeteer';
+
+class Sample {
+  run = async () => {
+    const browser = await puppeteer.launch();
+    const currentPage = (await browser.pages())[0];
+    await currentPage.goto('https://example.com');
+    await currentPage.screenshot({ path: 'example.png' });
+
+    await browser.close();
+  }
+}
+
+new Sample()
+  .run()
+  .then(() => console.log('Success'))
+  .catch(e => console.log('Fail', e));
